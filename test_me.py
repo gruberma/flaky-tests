311d74c96bfee4fd94d28fe567f9c012c65181c3
Martin Gruber <martin.gruber1@web.de>
2022-03-31 13:43:38 +0200

    Add random tests

--- test_me.py
+++ test_me.py
@@ -1,12 +1,25 @@
+import random
+import numpy as np
+
+
 def test_foo():
     assert True
 
-
-# Flaky due to non-deterministic builtin-function hash()
 def test_unordered_collections():
+    """
+    This test is flaky due to the non-deterministic builtin-function hash()
+    that controls the ordering within sets
+    """
     s = {"hello", "world"}
     assert next(s.__iter__()) == "hello"
 
+
+def test_random():
+    assert random.randint(0, 1) == 1
+
+
+def test_numpy_random():
+    assert np.random(0, 2) == 1
 
 x = 0;
 
